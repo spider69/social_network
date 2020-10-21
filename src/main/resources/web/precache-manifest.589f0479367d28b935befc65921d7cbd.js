743c4129256263f9cfe29fd5624fb1c6fd3a149a
YusupovPavel <yusalar@mail.ru>
2020-10-21 19:13:03 +0300

    web ui fix

---
+++ src/main/resources/web/precache-manifest.589f0479367d28b935befc65921d7cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5a29b9d23e6ffd8385552fd0e5fd9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "318088ad0f12eebb32cc",
+    "url": "/static/css/main.bfbbd90a.chunk.css"
+  },
+  {
+    "revision": "da653c1daf19256f1084",
+    "url": "/static/js/2.3afa3032.chunk.js"
+  },
+  {
+    "revision": "ae96ce37367c3b15926eb6b17d3aa94e",
+    "url": "/static/js/2.3afa3032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318088ad0f12eebb32cc",
+    "url": "/static/js/main.ed328a49.chunk.js"
+  },
+  {
+    "revision": "799ddb1bf19f0d8ebf54",
+    "url": "/static/js/runtime-main.f67ad493.js"
+  }
+]);
